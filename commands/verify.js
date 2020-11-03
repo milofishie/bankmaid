757b680735f83b21322bc434f68cfaa47b55e270
milofishie <69885222+milofishie@users.noreply.github.com>
2020-11-03 21:23:57 +0700

    Add files via upload

--- commands/verify.js
+++ commands/verify.js
@@ -6,14 +6,18 @@ module.exports = {
 	description: 'generate a random verification code',
 	execute(message, args) {
 		if (message.content.startsWith(`${prefix}verify`)) {
-   		var result = '';
-   		var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-   		var charactersLength = characters.length;
-			for (var i = 0; i < 10; i++) {
-            result += characters.charAt(Math.floor(Math.random() * charactersLength));
-   		}
-   		return message.reply(`**please type in \`${result}\` in the game (Say/Party Chat advised), and screenshot it.** Post the screenshot here and wait for one of our admins to verify your access to our Grand Market.\n\nNOTE: Your IGN must be visible, otherwise we will request you to repeat the process again.\n\n*Please be patient and wait for our admins to approve of your verification. This may take a few hours.*`);
-         console.log('done');
-		}
-	}
+         if(!args[0]){
+            return message.reply('you need to tell us your exact ign.')
+         }else{
+   		   var result = '';
+   		   var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+   	      var charactersLength = characters.length;
+			   for (var i = 0; i < 10; i++) {
+               result += characters.charAt(Math.floor(Math.random() * charactersLength));
+   		   }
+   		   return message.reply(` **please type in \`${result}\` in the game (Say/Party Chat advised), and screenshot it.** Post the screenshot here and wait for one of our admins to verify your access to our Grand Market.\n\nNOTE: Your IGN must be visible, otherwise we will request you to repeat the process again.\n\n*Please be patient and wait for our admins to approve of your verification. This may take a few hours.*`);
+            console.log('done');
+		   }
+	   }
+   }
 }
