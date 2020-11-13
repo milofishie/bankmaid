a866bccb29e8364977cb4e6b5eac1427fc1bfc1a
milofishie <69885222+milofishie@users.noreply.github.com>
2020-11-13 10:20:39 +0700

    Add files via upload

---
+++ verify.js
@@ -0,0 +1,62 @@
+const { prefix } = require('../config.json');
+const fsLibrary = require('fs');
+
+module.exports = {
+	name: 'verify',
+	description: 'generate a random verification code',
+	execute(message, args) {
+		if (message.content.startsWith(`${prefix}verify`)) {
+         if(!args[0]){
+            var result = '';
+            var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+            var charactersLength = characters.length;
+            for (var i = 0; i < 10; i++) {
+               result += characters.charAt(Math.floor(Math.random() * charactersLength));
+            }
+            return message.reply(` **please type in \`${result}\` in the game (Say/Party Chat advised), and screenshot it.** Post the screenshot here and wait for one of our admins to verify your access to our Grand Market.\n\nNOTE: Your IGN must be visible, otherwise we will request you to repeat the process again.\n\n*Please be patient and wait for our admins to approve of your verification. This may take a few hours.*`);
+         }else{
+            const taggedUser = message.mentions.members.first();
+            console.log(1);
+            var x = taggedUser.toString().replace('@!', '@');
+            var y = taggedUser.toString().replace('@', '@!');
+            console.log(2);
+            console.log(x);
+            console.log(args[0]);
+            console.log(taggedUser.toString())
+            if(args[0] == x || args[0] == taggedUser.toString() || args[0] == y){
+               x = taggedUser;
+               console.log(2.1);
+               if(message.member.hasPermission("MANAGE_ROLES" || "ADMINISTRATOR")){
+                  console.log(2.2);
+                  if(!args[1]){
+                     return message.reply(`you need to type the user's ign.`);
+                  }else{
+                     console.log(2.3);
+                     let data = '';
+                     for(var i = 0; i < args.length; i++){
+                        data += args[i].toString() + " ";
+                     }
+                     data += "\n";
+                     console.log(3)
+                     fsLibrary.appendFile("vrlist.txt", data, (error) => {
+                        if (error) throw err;
+                     })
+
+                     let role = message.guild.roles.cache.find(r => r.id === '772701612862734337');
+                     let member = x;
+                     member.roles.add(role).catch(console.log);
+                     console.log(4);
+                     return message.channel.send(`Verified ${taggedUser}.`)
+                     //console.log('Verification registered.')
+                  }  
+               }else{
+               return message.reply(`you don't have the permission to do that.`);
+               //console.log('noperm');
+               }
+            }else{
+               return message.channel.send('error');
+            }
+         }
+      }
+   }
+}
