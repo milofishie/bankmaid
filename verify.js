dc82888c8cec6d2bccef345758c3083328adaa28
milofishie <69885222+milofishie@users.noreply.github.com>
2020-11-17 15:03:55 +0700

    Add files via upload

--- verify.js
+++ verify.js
@@ -7,37 +7,24 @@ module.exports = {
 	execute(message, args) {
 		if (message.content.startsWith(`${prefix}verify`)) {
          if(!args[0]){
-            var result = '';
-            var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-            var charactersLength = characters.length;
-            for (var i = 0; i < 10; i++) {
-               result += characters.charAt(Math.floor(Math.random() * charactersLength));
-            }
-            return message.reply(` **please type in \`${result}\` in the game (Say/Party Chat advised), and screenshot it.** Post the screenshot here and wait for one of our admins to verify your access to our Grand Market.\n\nNOTE: Your IGN must be visible, otherwise we will request you to repeat the process again.\n\n*Please be patient and wait for our admins to approve of your verification. This may take a few hours.*`);
+            message.reply(`you need to tell us your in game name.`);
          }else{
             const taggedUser = message.mentions.members.first();
-            console.log(1);
+            //console.log(args[0]);
             var x = taggedUser.toString().replace('@!', '@');
-            var y = taggedUser.toString().replace('@', '@!');
-            console.log(2);
-            console.log(x);
-            console.log(args[0]);
-            console.log(taggedUser.toString())
-            if(args[0] == x || args[0] == taggedUser.toString() || args[0] == y){
+
+            if(args[0] == x || args[0] == taggedUser.toString()){
                x = taggedUser;
-               console.log(2.1);
                if(message.member.hasPermission("MANAGE_ROLES" || "ADMINISTRATOR")){
-                  console.log(2.2);
                   if(!args[1]){
                      return message.reply(`you need to type the user's ign.`);
                   }else{
-                     console.log(2.3);
                      let data = '';
                      for(var i = 0; i < args.length; i++){
                         data += args[i].toString() + " ";
                      }
                      data += "\n";
-                     console.log(3)
+                     //console.log(arguments.length)
                      fsLibrary.appendFile("vrlist.txt", data, (error) => {
                         if (error) throw err;
                      })
@@ -45,7 +32,7 @@ module.exports = {
                      let role = message.guild.roles.cache.find(r => r.id === '772701612862734337');
                      let member = x;
                      member.roles.add(role).catch(console.log);
-                     console.log(4);
+
                      return message.channel.send(`Verified ${taggedUser}.`)
                      //console.log('Verification registered.')
                   }  
@@ -54,7 +41,13 @@ module.exports = {
                //console.log('noperm');
                }
             }else{
-               return message.channel.send('error');
+               var result = '';
+               var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+               var charactersLength = characters.length;
+               for (var i = 0; i < 10; i++) {
+                  result += characters.charAt(Math.floor(Math.random() * charactersLength));
+               }
+               return message.reply(` **please type in \`${result}\` in the game (Say/Party Chat advised), and screenshot it.** Post the screenshot here and wait for one of our admins to verify your access to our Grand Market.\n\nNOTE: Your IGN must be visible, otherwise we will request you to repeat the process again.\n\n*Please be patient and wait for our admins to approve of your verification. This may take a few hours.*`);
             }
          }
       }
