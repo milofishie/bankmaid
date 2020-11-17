7b6115cfec22955bef530aac8e6669bd48cdd04d
milofishie <69885222+milofishie@users.noreply.github.com>
2020-11-17 15:18:11 +0700

    Add files via upload

--- commands/verify.js
+++ commands/verify.js
@@ -7,18 +7,12 @@ module.exports = {
 	execute(message, args) {
 		if (message.content.startsWith(`${prefix}verify`)) {
          if(!args[0]){
-            var result = '';
-            var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-            var charactersLength = characters.length;
-            for (var i = 0; i < 10; i++) {
-               result += characters.charAt(Math.floor(Math.random() * charactersLength));
-            }
-            return message.reply(` **please type in \`${result}\` in the game (Say/Party Chat advised), and screenshot it.** Post the screenshot here and wait for one of our admins to verify your access to our Grand Market.\n\nNOTE: Your IGN must be visible, otherwise we will request you to repeat the process again.\n\n*Please be patient and wait for our admins to approve of your verification. This may take a few hours.*`);
+            message.reply(`you need to tell us your in game name.`);
          }else{
             const taggedUser = message.mentions.members.first();
             //console.log(args[0]);
             var x = taggedUser.toString().replace('@!', '@');
-            console.log(x);
+
             if(args[0] == x || args[0] == taggedUser.toString()){
                x = taggedUser;
                if(message.member.hasPermission("MANAGE_ROLES" || "ADMINISTRATOR")){
@@ -46,6 +40,14 @@ module.exports = {
                return message.reply(`you don't have the permission to do that.`);
                //console.log('noperm');
                }
+            }else{
+               var result = '';
+               var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+               var charactersLength = characters.length;
+               for (var i = 0; i < 10; i++) {
+                  result += characters.charAt(Math.floor(Math.random() * charactersLength));
+               }
+               return message.reply(` **please type in \`${result}\` in the game (Say/Party Chat advised), and screenshot it.** Post the screenshot here and wait for one of our admins to verify your access to our Grand Market.\n\nNOTE: Your IGN must be visible, otherwise we will request you to repeat the process again.\n\n*Please be patient and wait for our admins to approve of your verification. This may take a few hours.*`);
             }
          }
       }
