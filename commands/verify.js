59e39d2827b75bee0c661aee3195f3e0be5587dc
milofishie <69885222+milofishie@users.noreply.github.com>
2020-11-17 20:07:57 +0700

    Add files via upload

--- commands/verify.js
+++ commands/verify.js
@@ -6,7 +6,57 @@ module.exports = {
 	description: 'generate a random verification code',
 	execute(message, args) {
 		if (message.content.startsWith(`${prefix}verify`)) {
-			message.channel.send('Shut up this command is broken!');
-		}
-	}
+         if(!args[0]){
+            message.reply(`you need to tell us your in game name.`);
+         }else{
+            const taggedUser = message.mentions.members.first();
+            
+            var x = `${taggedUser}`.toString().replace('@!', '@');
+            var y = `${taggedUser}`.toString().replace('@', '@!');
+            console.log(args[0]);
+            console.log(1);
+            //console.log(taggedUser);
+            console.log(x);
+            console.log(2);
+            //console.log(taggedUser.toString());
+
+            if(args[0] == x || args[0] == `${taggedUser}`.toString() || args[0] == y){
+               x = taggedUser;
+               if(message.member.hasPermission("MANAGE_ROLES" || "ADMINISTRATOR")){
+                  if(!args[1]){
+                     return message.reply(`you need to type the user's ign.`);
+                  }else{
+                     let data = '';
+                     for(var i = 0; i < args.length; i++){
+                        data += args[i].toString() + " ";
+                     }
+                     data += "\n";
+                     //console.log(arguments.length)
+                     fsLibrary.appendFile("vrlist.txt", data, (error) => {
+                        if (error) throw err;
+                     })
+
+                     let role = message.guild.roles.cache.find(r => r.id === '772701612862734337');
+                     let member = x;
+                     member.roles.add(role).catch(console.log);
+
+                     return message.channel.send(`Verified ${taggedUser}.`)
+                     //console.log('Verification registered.')
+                  }  
+               }else{
+               return message.reply(`you don't have the permission to do that.`);
+               //console.log('noperm');
+               }
+            }else{
+               var result = '';
+               var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+               var charactersLength = characters.length;
+               for (var i = 0; i < 10; i++) {
+                  result += characters.charAt(Math.floor(Math.random() * charactersLength));
+               }
+               return message.reply(` **please type in \`${result}\` in the game (Say/Party Chat advised), and screenshot it.** Post the screenshot here and wait for one of our admins to verify your access to our Grand Market.\n\nNOTE: Your IGN must be visible, otherwise we will request you to repeat the process again.\n\n*Please be patient and wait for our admins to approve of your verification. This may take a few hours.*`);
+            }
+         }
+      }
+   }
 }
